0239f2ee63ac98f048f2924aa40de77df388156f
April <569551020@qq.com>
2023-10-15 23:43:56 +0800

    [NoOverscrollBehaviorMixin]新增[needScrollbar]以控制是否构建滚动条

--- lib/src/custom/scrollable/scroll_behavior.dart
+++ lib/src/custom/scrollable/scroll_behavior.dart
@@ -4,6 +4,17 @@ mixin NoOverscrollBehaviorMixin on ScrollBehavior {
   ///不使用过度滚动效果的方向
   List<AxisDirection> get disallowedDirections;
 
+  ///是否需要 Scrollbar
+  bool get needScrollbar;
+
+  @override
+  Widget buildScrollbar(BuildContext context, Widget child, ScrollableDetails details) {
+    if (!needScrollbar) {
+      return child;
+    }
+    return super.buildScrollbar(context, child, details);
+  }
+
   @override
   Widget buildOverscrollIndicator(BuildContext context, Widget child, ScrollableDetails details) {
     //不使用任何过度滚动效果
@@ -20,11 +31,15 @@ mixin NoOverscrollBehaviorMixin on ScrollBehavior {
 }
 
 ///允许设置不使用过度滚动效果的滚动行为配置类
-class NoOverscrollScrollBehavior extends ScrollBehavior with NoOverscrollBehaviorMixin {
+class NoOverscrollScrollBehavior extends MaterialScrollBehavior with NoOverscrollBehaviorMixin {
   const NoOverscrollScrollBehavior({
     this.disallowedDirections = AxisDirection.values,
+    this.needScrollbar = false,
   }) : super();
 
   @override
   final List<AxisDirection> disallowedDirections;
+
+  @override
+  final bool needScrollbar;
 }
